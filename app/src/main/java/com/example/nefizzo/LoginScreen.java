85cf5342038c12a2044845b85f0259a4edf83996
elifgoral <elif-goral99@hotmail.com>
2020-11-17 22:01:55 +0300

    login-sign and profile added by Elif Goral

---
+++ app/src/main/java/com/example/nefizzo/LoginScreen.java
@@ -0,0 +1,102 @@
+package com.example.nefizzo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginScreen extends AppCompatActivity {
+
+    Button loginButton,forgotButton,createAccountButton;
+    EditText mail,password;
+    String mailtxt,passwordtxt;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+        mAuth = FirebaseAuth.getInstance();
+        tanimla();
+        goNewActivity();
+    }
+
+    public void tanimla(){
+            loginButton = findViewById(R.id.loginButton);
+            forgotButton = findViewById(R.id.forgotButton);
+            createAccountButton = findViewById(R.id.createAccountButton);
+            mail = findViewById(R.id.mailEditText);
+            password = findViewById(R.id.passwordEditText);
+    }
+
+    public void degerAl(){
+            mailtxt = mail.getText().toString();
+            passwordtxt = password.getText().toString();
+    }
+
+    public void goNewActivity(){
+            forgotButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    goResetScreen();
+                }
+            });
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    goMainScreen();
+                }
+            });
+            createAccountButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    goSignScreen();
+                }
+            });
+    }
+
+    public void goResetScreen(){
+                Intent intent = new Intent(getApplicationContext(),ResetPassword.class);
+                startActivity(intent);
+    }
+
+    public void goMainScreen(){
+                degerAl();
+                user = mAuth.getCurrentUser();
+                mAuth.signInWithEmailAndPassword(mailtxt, passwordtxt).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(LoginScreen.this, "You login Succesfully", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), Profile.class);
+                        startActivity(intent);
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginScreen.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    public void goSignScreen(){
+                Intent intent = new Intent(getApplicationContext(),SignActivity.class);
+                startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/nefizzo/Member.java
@@ -0,0 +1,131 @@
+package com.example.nefizzo;
+
+public class Member {
+
+    /**
+     * username of member
+     */
+    private String username;
+
+    /**
+     * Name of member
+     */
+    private String name;
+    /**
+     * Surname of member
+     */
+    private String surname;
+    /**
+     * mail address of member
+     */
+    private String mailAddress;
+    /**
+     * password of member
+     */
+    private String password;
+
+    /**
+     * gender of member
+     */
+    private String gender;
+
+
+    /**
+     * That method returns the gender.
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * That methods set the gender.
+     * @param gender member's gender.
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * That method returns the username.
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * That methods set the name.
+     * @param username member's username.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * That method returns the name.
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * That methods set the name.
+     * @param name member's name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * That method returns the surname.
+     * @return surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * That method set the surname.
+     * @param surname
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * That method returns the mail address.
+     * @return mail address of member.
+     */
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    /**
+     *  That method sets the mail address.
+     * @param mailAddress member's mail address.
+     */
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    /**
+     * That method returns the password.
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * That method sets the password.
+     * @param password member's password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
