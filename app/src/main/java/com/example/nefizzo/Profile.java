3ab956581db5d51309e362ffbcb58e45ce8c1a6a
saliha <salihaderin@gmail.com>
2020-12-14 19:44:48 +0300

    Profile updated

--- app/src/main/java/com/example/nefizzo/Profile.java
+++ app/src/main/java/com/example/nefizzo/Profile.java
@@ -3,9 +3,16 @@ package com.example.nefizzo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,30 +24,63 @@ import java.util.ArrayList;
 
 public class Profile extends AppCompatActivity {
 
-    TextView mail,name,surname,username,gender,password;
-    DatabaseReference userRef;
+    TextView mail,name,surname,username,gender;
+    Button recipeButton;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference usersRef;
+    FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
         defineValues();
+        readData();
+        recipesButton();
+    }
+
+    public void recipesButton(){
+        recipeButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RecipeListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void defineValues(){
         username = findViewById(R.id.usernameProfil);
         name = findViewById(R.id.nameProfil);
         surname = findViewById(R.id.surnameProfil);
-        password = findViewById(R.id.passwordProfil);
         mail = findViewById(R.id.mailProfil);
         gender = findViewById(R.id.genderProfil);
+        recipeButton = findViewById(R.id.recipes);
     }
 
     public void readData() {
-        userRef = FirebaseDatabase.getInstance().getReference("Members");
-        userRef.addValueEventListener(new ValueEventListener() {
+        usersRef = FirebaseDatabase.getInstance().getReference("Members");
+        usersRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String BufferUserName,bufferName,bufferSurname,BufferGender;
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    if(user.getEmail().equals(ds.child("mailAddress").getValue().toString())){
+                        BufferUserName = ds.child("username").getValue().toString();
+                        bufferName = ds.child("name").getValue().toString();
+                        bufferSurname = ds.child("surname").getValue().toString();
+                        BufferGender = ds.child("gender").getValue().toString();
+                        username.setText(BufferUserName);
+                        name.setText(bufferName);
+                        surname.setText(bufferSurname);
+                        mail.setText(user.getEmail());
+                        gender.setText(BufferGender);
+                        return;
+                    }
+                }
             }
 
             @Override
