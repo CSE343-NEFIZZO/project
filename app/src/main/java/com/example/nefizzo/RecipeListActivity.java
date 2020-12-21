173a8dc7a5c7b99ff0e3242c981e7e6eaa313e73
saliha <salihaderin@gmail.com>
2020-12-21 23:08:08 +0300

    Recipe detail added Remove added

--- app/src/main/java/com/example/nefizzo/RecipeListActivity.java
+++ app/src/main/java/com/example/nefizzo/RecipeListActivity.java
@@ -35,7 +35,6 @@ public class RecipeListActivity extends AppCompatActivity {
     FirebaseUser user;
     String username;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
