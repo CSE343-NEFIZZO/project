a2a9ffd5840582475c5069cf699303b3d6e9a591
Emirhan Uzun <emirhan_uzun_1907@hotmail.com>
2020-12-28 14:37:41 +0300

    Add Recipe updated

--- app/src/main/java/com/example/nefizzo/AddRecipeActivity.java
+++ app/src/main/java/com/example/nefizzo/AddRecipeActivity.java
@@ -108,7 +108,7 @@ public class AddRecipeActivity extends AppCompatActivity {
             if (urlImage != null) {
                 imageUrl = urlImage.toString();
             }
-            uploadRecipe(userName2);
+            //uploadRecipe(userName2);
             Toast.makeText(AddRecipeActivity.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
         });
     }
@@ -152,6 +152,7 @@ public class AddRecipeActivity extends AppCompatActivity {
                         if(user.getEmail().equals(ds.child("mailAddress").getValue().toString())){
                             userName2 = ds.child("username").getValue().toString();
                             uploadImage(userName2);
+                            uploadRecipe(userName2);
                             return;
                         }
 
@@ -194,6 +195,8 @@ public class AddRecipeActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     Toast.makeText(AddRecipeActivity.this,"Recipe Uploaded",Toast.LENGTH_LONG).show();
                     progressDialog.dismiss();
+                    Intent intent = new Intent(AddRecipeActivity.this,MainScreen.class);
+                    startActivity(intent);
                     finish();
                 }
 
@@ -202,10 +205,12 @@ public class AddRecipeActivity extends AppCompatActivity {
                 progressDialog.dismiss();
 
             });
+
+
         }
 
 
 
 
-
 }
+
